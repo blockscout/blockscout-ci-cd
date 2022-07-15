a6e55e32e197b7b67ce60b323c48c089c44b1b0d
0xtzData <108135703+0xtzData@users.noreply.github.com>
2022-07-15 18:08:52 +0200

    test ingress (#7)

--- blockscout/main.ts
+++ blockscout/main.ts
@@ -1,7 +1,6 @@
 import { App } from 'cdk8s'
 import { K8sClient } from './client/client'
-import { env } from './config'
-import { BlockscoutChart } from './pkg/blockscout'
+import { BlockscoutChart, ResourceMode } from './pkg/blockscout'
 
 const app = new App()
 const chart = new BlockscoutChart(
@@ -10,13 +9,33 @@ const chart = new BlockscoutChart(
     {},
     {
         // TODO: do spread with a prefix for env vars
-        image: env.IMAGE || `blockscout/blockscout:latest`,
-        namespaceName: env.NAMESPACE_NAME,
-        wallet: env.WALLET,
-        httpURL: env.HTTP_URL,
-        wsURL: env.WS_URL,
-        variant: env.VARIANT,
-        resourceMode: env.RESOURCE_MODE,
+        image: process.env.IMAGE || `blockscout/blockscout:latest`,
+        namespaceName: process.env.NAMESPACE_NAME || `e2e-test`,
+        wallet: process.env.WALLET || ``,
+        httpURL: process.env.HTTP_URL || `http://localhost:8544`,
+        wsURL: process.env.WS_URL || `ws://localhost:8546`,
+        httpTraceURL: process.env.HTTP_URL || `http://localhost:8544`,
+        variant: process.env.VARIANT || `geth`,
+        coin: process.env.COIN || `DAI`,
+        resourceMode: process.env.RESOURCE_MODE || ResourceMode.E2E,
+        // mainnet vars
+        firstBlock: process.env.FIRST_BLOCK,
+        lastBlock: process.env.LAST_BLOCK,
+        port: process.env.PORT,
+        auth0Domain: process.env.AUTH0_DOMAIN,
+        auth0clientID: process.env.AUTH0_CLIENT_ID,
+        auth0clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        auth0CallbackURL: process.env.AUTH0_CALLBACK_URL,
+        auth0LogoutReturnURL: process.env.AUTH0_LOGOUT_RETURN_URL,
+        auth0LogoutURL: process.env.AUTH0_LOGOUT_URL,
+        sendGridAPIKey: process.env.SENDGRID_API_KEY,
+        sendGridSender: process.env.SENDGRID_SENDER,
+        sendGridTemplate: process.env.SENDGRID_TEMPLATE,
+        publicTagsAirtable: process.env.PUBLIC_TAGS_AIRTABLE_URL,
+        publicTagsAirtableAPIKey: process.env.PUBLIC_TAGS_AIRTABLE_API_KEY,
+        socketRoot: process.env.SOCKET_ROOT,
+        networkPath: process.env.NETWORK_PATH,
+        secretKeyBase: process.env.SECRET_KEY_BASE,
     },
 )
 app.synth()
