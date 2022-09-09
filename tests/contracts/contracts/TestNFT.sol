c3d9462300c12788b61e1dc18858eeea68c12b35
0xtzData <sergei@blockscout.com>
2022-09-09 13:44:16 +0200

    update tests/vars

--- tests/contracts/contracts/TestNFT.sol
+++ tests/contracts/contracts/TestNFT.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
