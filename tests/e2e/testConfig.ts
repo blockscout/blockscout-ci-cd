881681746e23309716907811c6be7015cc393f31
0xtzData <sergei@blockscout.com>
2022-10-13 14:42:15 +0200

    skip bugged verification

--- tests/e2e/testConfig.ts
+++ tests/e2e/testConfig.ts
@@ -3,6 +3,7 @@ export default {
     test: process.env.BLOCKSCOUT_URL,
     networkURL: process.env.NETWORK_URL,
     waitForElement: 120000,
+    waitForVerification: 300000,
     dbUsername: ``,
     dbPassword: ``,
     dbServerName: ``,
