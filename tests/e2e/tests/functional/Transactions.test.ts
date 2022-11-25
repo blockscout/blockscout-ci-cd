79b46ad1859f9afee6295355591a6866d4e3186e
0xtzData <sergei@blockscout.com>
2022-11-25 15:57:34 +0100

    trigger again

--- tests/e2e/tests/functional/Transactions.test.ts
+++ tests/e2e/tests/functional/Transactions.test.ts
@@ -3,7 +3,7 @@ import test from '@lib/BaseTest'
 
 test.describe.configure({ mode: `parallel` })
 
-test(`@Ethereum @Transactions @Data @AccountImage Check transactions list (new frontend)`, async ({ transactionsListPage }) => {
+test(`@Ethereum @Transactions @Data @AccountImage Check transactions list`, async ({ transactionsListPage }) => {
     const {
         TestNFTSymbolV,
         TestTokenSymbolV,
@@ -15,7 +15,7 @@ test(`@Ethereum @Transactions @Data @AccountImage Check transactions list (new f
     await transactionsListPage.check_table_data(TestNFTSymbolV, TestNFTSymbol, TestTokenSymbolV, TestTokenSymbol)
 })
 
-test(`@Ethereum @Transactions @Data @AccountImage Check contract creation tx props (new frontend)`, async ({ transactionPage }) => {
+test(`@Ethereum @Transactions @Data @AccountImage Check contract creation tx props`, async ({ transactionPage }) => {
     const { TestTokenDeployTXHash, MinerAddress, TestTokenAddress } = process.env
     await transactionPage.open(TestTokenDeployTXHash)
     await transactionPage.check_tx_description()
