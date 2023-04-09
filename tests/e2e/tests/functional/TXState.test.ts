8dceaca0586474928f1505bce44c00859e29afb7
0xtzData <sergei@blockscout.com>
2023-04-09 15:04:12 +0200

    fix

--- tests/e2e/tests/functional/TXState.test.ts
+++ tests/e2e/tests/functional/TXState.test.ts
@@ -2,7 +2,6 @@ import test from '@lib/BaseTest'
 
 test.describe.configure({ mode: `parallel` })
 
-test(`@Rollup Eth main page components`, async ({ context, newHomeMainDev }) => {
+test(`TXState change`, async ({ context, newHomeMainDev }) => {
     await newHomeMainDev.open()
-    await ethHomePage.verifyComponents(context)
 })
