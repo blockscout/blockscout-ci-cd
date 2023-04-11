4ea488bc80bab5257598be50b64b20c0efcc8e4c
0xtzData <sergei@blockscout.com>
2023-04-11 18:37:39 +0200

    tx state first case

--- tests/e2e/pages/HomeMainDev.ts
+++ tests/e2e/pages/HomeMainDev.ts
@@ -7,10 +7,16 @@ export class HomeMainDev extends HomePage {
 
     actions: WebActions
 
+    ROW = `main >> tr >> nth={}`
+
     constructor(page: Page) {
         super(page)
         this.page = page
         this.actions = new WebActions(this.page)
+    }
+
+    row(num: number): string {
+        return `main >> tr >> nth=${num}`
     }
 
     async open(): Promise<void> {
@@ -19,5 +25,9 @@ export class HomeMainDev extends HomePage {
 
     async open_state_change(tx: string): Promise<void> {
         await this.actions.navigateToURL(`https://blockscout-main.test.aws-k8s.blockscout.com/tx/${tx}?tab=state`)
+    }
+
+    async assert_row(num: number, text: string): Promise<void> {
+        await this.actions.verifyElementContainsText(this.row(num), text)
     }
 }
