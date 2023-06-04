8b21b0a84418e1eef98ce7ae12a0d0bd73e69905
0xtzData <sergei@blockscout.com>
2023-06-04 18:39:22 +0200

    lower timeout

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -14,10 +14,10 @@ const config: PlaywrightTestConfig = {
     globalTeardown: `./global-teardown`,
 
     // sets timeout for each test case
-    timeout: 300000,
+    timeout: 30000,
 
     // number of retries if test case fails
-    retries: 0,
+    retries: 3,
 
     // Reporters
     reporter: [[`list`], [`html`, { outputFolder: `html-report`, open: `never` }], [`junit`, { outputFile: `results.xml` }]],
