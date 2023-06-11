39765122d05316d358e52ff4a5b3e83f5dae833f
0xtzData <sergei@blockscout.com>
2023-06-11 09:13:48 +0200

    semi-auto tests for verified addrs

---
+++ tests/e2e/tests/functional/AddressOwners.api.test.ts
@@ -0,0 +1,71 @@
+/* eslint-disable dot-notation */
+import test from '@lib/BaseTest'
+import Contracts from '@lib/Contracts'
+import { expect } from '@playwright/test'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { TransactionResponse } from '@ethersproject/abstract-provider'
+import { ethers } from 'ethers'
+import { TestToken } from '../../../contracts/typechain/contracts/TestToken'
+import { TestNFT } from '../../../contracts/typechain/contracts/TestNFT'
+
+test.describe.configure({ mode: `parallel` })
+
+const RECEIPT_RETRIES = 5
+
+const shortID = (): string => {
+    // eslint-disable-next-line no-bitwise
+    let firstPart = (Math.random() * 46656) | 0
+    // eslint-disable-next-line no-bitwise
+    let secondPart = (Math.random() * 46656) | 0
+    firstPart = (`000${firstPart.toString(36)}`).slice(-3) as any
+    secondPart = (`000${secondPart.toString(36)}`).slice(-3) as any
+    const r = firstPart + secondPart
+    return r.toString()
+}
+
+const waitReceiptWithBlock = async (provider: JsonRpcProvider, hash: string): Promise<TransactionResponse> => {
+    for (let i = 0; i < RECEIPT_RETRIES; i += 1) {
+        // eslint-disable-next-line no-await-in-loop
+        const r = await provider.getTransaction(hash)
+        if (r.blockNumber) {
+            return r
+        }
+    }
+    throw Error(`tx ${hash} is not mined in any block`)
+}
+
+const deployContract = async (contracts: Contracts, artifactName: string, contractName: string, contractSymbol: string): Promise<TestToken | TestNFT> => {
+    console.log(`deploying contract`)
+    const token = await contracts.deploySymbolContract(contractName, contractSymbol, artifactName) as TestToken
+    await waitReceiptWithBlock(contracts.provider, token.deployTransaction.hash)
+    return token
+}
+
+// manual on live/testnet networks
+
+test.skip(`@AddressOwners Deploy ownable ERC20 token, verify addrress`, async () => {
+    const contracts = new Contracts(process.env.NETWORK_URL)
+
+    const token = await deployContract(
+        contracts,
+        `TestToken`,
+        `VerifiedByCreatorContract`,
+        shortID(),
+    )
+    await token.mint(contracts.wallet.address, 100)
+})
+
+test.skip(`@AddressOwners Deploy ownable ERC20 token, manual signature verification`, async () => {
+    const contracts = new Contracts(process.env.NETWORK_URL)
+
+    const token = await deployContract(
+        contracts,
+        `TestToken`,
+        `VerifiedByManualSig`,
+        shortID(),
+    )
+    await token.mint(contracts.wallet.address, 100)
+
+    const signature = await contracts.wallet.signMessage(`[eth-goerli.blockscout.com] [2023-06-10 23:09:26] I, hereby verify that I am the owner/creator of the address [0xc0d74a6e896fb8e258d2542c2629f3e06dec824c]`)
+    console.log(`signature: ${signature}`)
+})
