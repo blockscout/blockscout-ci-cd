33a126a4f6a9ba5e56abe865c0d5f0bd864d483a
0xtzData <sergei@blockscout.com>
2024-03-21 13:23:09 +0100

    don't wait for network, page is broken

--- tests/e2e/pages/Login.ts
+++ tests/e2e/pages/Login.ts
@@ -308,7 +308,7 @@ export class AuthorizedArea extends CommonPage {
         await this.page.reload()
         await this.actions.clickElement(this.DELETE_ROW_ICON)
         await this.actions.clickElement(this.SAVE_BTN)
-        await this.page.waitForLoadState(`networkidle`)
+        await this.actions.delay(5000)
     }
 
     async addAddressWatch(data: WatchListSpec): Promise<void> {
