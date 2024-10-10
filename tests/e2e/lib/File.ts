4574c346867672e8ef8a6f7f7d526e9e57aee10c
0xtzData <sergei@blockscout.com>
2024-10-10 12:29:03 +0200

    remove api test from live

--- tests/e2e/lib/File.ts
+++ tests/e2e/lib/File.ts
@@ -1,6 +1,6 @@
-import {readFileSync} from "fs"
+import { readFileSync } from "fs"
 import chalk from "chalk"
-import {APIRequestContext, expect, request} from "@playwright/test";
+import { APIRequestContext, expect, request } from "@playwright/test"
 
 export const LoadDataFile = (url: string): any => {
     if (process.env.ENV === `test` || process.env.ENV === `scoutcloud`) {
@@ -15,44 +15,42 @@ export const LoadDataFile = (url: string): any => {
     }
 }
 
-const newReqCtx = async (url: string) => {
-    return await request.newContext({
-        baseURL: url,
-        extraHTTPHeaders: {},
-    })
-}
+const newReqCtx = async (url: string) => await request.newContext({
+    baseURL: url,
+    extraHTTPHeaders: {},
+})
 
 export const paginationToQuery = (params: { [key: string]: any }): string => {
     const filteredParams = Object.entries(params)
         .map(([key, value]) => {
             // If value is undefined or an empty string, set it explicitly as null
-            if (value === undefined || value === '') {
-                value = 'null';
+            if (value === undefined || value === ``) {
+                value = `null`
             }
-            return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
-        });
-    return filteredParams.length ? `${filteredParams.join('&')}` : '';
+            return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+        })
+    return filteredParams.length ? `${filteredParams.join(`&`)}` : ``
 }
 
 export const LoadTokens = async (url: string, type: string, pages: number): Promise<any> => {
     const r = await newReqCtx(url)
-    let data = {
-        tokens: []
+    const data = {
+        tokens: [],
     }
-    if (type !== 'ERC-20' && type !== 'ERC-721' && type !== 'ERC-1155' && type !== 'ERC-404') {
-        throw Error('invalid types, allowed types are ERC-20/721/1155/404')
+    if (type !== `ERC-20` && type !== `ERC-721` && type !== `ERC-1155` && type !== `ERC-404`) {
+        throw Error(`invalid types, allowed types are ERC-20/721/1155/404`)
     }
-    let pageParams = ''
-    let urlWithParams = ''
+    let pageParams = ``
+    let urlWithParams = ``
     for (let i = 0; i < pages; i++) {
         urlWithParams = `${url}/api/v2/tokens?type=${type}&${pageParams}`
         // console.log(`url with params: ${urlWithParams}`)
-        const resp = await r.get(urlWithParams);
-        expect(resp.ok()).toBeTruthy();
-        expect(resp.status()).toBe(200);
-        const body = await resp.json();
-        pageParams = paginationToQuery(body['next_page_params']);
-        data.tokens.push(...body['items']);
+        const resp = await r.get(urlWithParams)
+        expect(resp.ok()).toBeTruthy()
+        expect(resp.status()).toBe(200)
+        const body = await resp.json()
+        pageParams = paginationToQuery(body.next_page_params)
+        data.tokens.push(...body.items)
     }
     return data
 }
--- tests/e2e/tests/api/api.test.ts
+++ tests/e2e/tests/api/api.test.ts
@@ -1,18 +1,18 @@
-import {test, expect} from '@playwright/test'
-import {LoadDataFile, LoadTokens } from "@lib/File"
+import { test, expect } from '@playwright/test'
+import { LoadDataFile, LoadTokens } from "@lib/File"
 
 const urls = (process.env.BLOCKSCOUT_URL || ``).split(`,`)
 
 urls.forEach((url: string) => {
     LoadDataFile(url)
     test(`@Api ${url} Check tokens data`, async ({ request }): Promise<void> => {
-        const data = await LoadTokens(url,'ERC-1155', 3)
+        const data = await LoadTokens(url, `ERC-1155`, 3)
         for (const t of data.tokens) {
             console.log(`addr: ${t.address}`)
-            const resp = await request.get(`${url}/api/v2/tokens/${t.address}`);
-            expect(resp.ok()).toBeTruthy();
-            expect(resp.status()).toBe(200);
-            const body = await resp.json();
+            const resp = await request.get(`${url}/api/v2/tokens/${t.address}`)
+            expect(resp.ok()).toBeTruthy()
+            expect(resp.status()).toBe(200)
+            const body = await resp.json()
             console.log(`body: ${JSON.stringify(body, null, 2)}`)
         }
     })
