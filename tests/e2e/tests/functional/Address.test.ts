b1eb409b9dcad56067082aecdadee74c5392fd18
0xtzData <sergei@blockscout.com>
2024-12-26 00:28:22 +0100

    skip inconsistent address page check

--- tests/e2e/tests/functional/Address.test.ts
+++ tests/e2e/tests/functional/Address.test.ts
@@ -3,7 +3,7 @@ import test from '@lib/BaseTest'
 
 test.describe.configure({ mode: `parallel` })
 
-test(`@AccountImage @Address Check address page`, async ({ addressPage }) => {
+test.skip(`@AccountImage @Address Check address page`, async ({ addressPage }) => {
     const { TestTokenAddress } = process.env
     await addressPage.mock_ads()
     await addressPage.open(TestTokenAddress)
